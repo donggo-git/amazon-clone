2d70f7074137bc0832302f939f6ef84b94af6f21
Dong <dlttthvlhhth@gmail.com>
2021-04-07 22:23:59 -0700

    complete the fundamental of header

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './Header.css'
 import styled from 'styled-components'
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 const Container = styled.div`
 height:60px;
 background-color:#0F1111;
 display:flex;
 align-items: center;
-justify-content: center
+
+color: white
 `
 
 const HeaderLogo = styled.div`
@@ -16,12 +19,19 @@ img{
     margin-left:11px;
 }
 `
-
+const HeaderSearch = styled.div``
+const HeaderSearchInput = styled.input``
 const HeaderOptionAddress = styled.div``
+const HeaderSearchIconContainer = styled.div``
 
 const OptionLineOne = styled.div``
-
 const OptionLineTwo = styled.div``
+
+const HeaderNavItems = styled.div`
+display:flex`
+const HeaderOption = styled.div``
+const CartCount = styled.div``
+
 function Header() {
     return (
         <Container>
@@ -32,6 +42,29 @@ function Header() {
                 <OptionLineOne>Hello</OptionLineOne>
                 <OptionLineTwo>Select your address</OptionLineTwo>
             </HeaderOptionAddress>
+
+            <HeaderSearch>
+                <HeaderSearchInput type="text" />
+                <HeaderSearchIconContainer>
+                    <SearchIcon />
+                </HeaderSearchIconContainer>
+            </HeaderSearch>
+
+            <HeaderNavItems>
+                <HeaderOption>
+                    <OptionLineOne>Hello, Dong</OptionLineOne>
+                    <OptionLineTwo>Account & List</OptionLineTwo>
+                </HeaderOption>
+                <HeaderOption>
+                    <OptionLineOne>Returns</OptionLineOne>
+                    <OptionLineTwo>&Orders</OptionLineTwo>
+                </HeaderOption>
+                <HeaderOption>
+                    <ShoppingBasketIcon />
+                    <CartCount>5</CartCount>
+                </HeaderOption>
+            </HeaderNavItems>
+
         </Container>
     )
 }

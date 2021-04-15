3f1b3df9e32a13eec7ac31b42f07874d073f9f52
Dong <dlttthvlhhth@gmail.com>
2021-04-14 23:30:29 -0700

    add style for product

--- src/Product.js
+++ src/Product.js
@@ -5,29 +5,53 @@ function Product() {
     return (
         <Container>
             <Title>
+                Ipad Pro
             </Title>
             <Price>
-
+                $1449
             </Price>
-            <Rating></Rating>
-            <Img />
-            <AddToCartButton>
-                Add to cart
+            <Rating>
+
+            </Rating>
+            <Img src="" />
+            <ActionSection>
+                <AddToCartButton>
+                    Add to cart
             </AddToCartButton>
-        </Container>
+            </ActionSection>
+        </Container >
     )
 }
 const Container = styled.div`
     background-color:white;
     z-index:100;
-    height:300px;
+    max-height:400px;
     flex:1;
     padding:20px;
     margin:10px;
+    display:flex;
+    flex-direction: column;
 `
-const Title = styled.p``;
-const Price = styled.p``;
+const Title = styled.span``;
+const Price = styled.span`
+    font-weight:500;
+    margin-top: 3px;
+`;
 const Rating = styled.div``;
-const Img = styled.img``;
-const AddToCartButton = styled.button``
+const Img = styled.img`
+max-height:200px;
+object-fit:contain;
+`;
+const ActionSection = styled.div`
+display:grid;
+place-items: center;
+`;
+const AddToCartButton = styled.button`
+width:100px;
+height:30px;
+background-color:#f0c14b;
+border:2px solid #a88734;
+border-radius:2px;
+margin: 0 auto;
+`
 export default Product

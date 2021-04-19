4874cd2cc91ff78cfe5f65f53046c44874707978
Dong <dlttthvlhhth@gmail.com>
2021-04-18 23:04:48 -0700

    install firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
+import firebase from 'firebase'
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseConfig = firebase.initializeApp({
     apiKey: "AIzaSyBVS0hAY0AWY3AqQmz1X3fDna17dy4Pm7I",
     authDomain: "clone-51be6.firebaseapp.com",
     projectId: "clone-51be6",
@@ -7,4 +9,5 @@ const firebaseConfig = {
     messagingSenderId: "643503684111",
     appId: "1:643503684111:web:034e5bab3cd24ba37b817a",
     measurementId: "G-XLM7NEWJLY"
-};
+});
+const db = firebase.firestore()

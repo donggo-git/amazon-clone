aec5ac0d51ad25338e15c6245dfbb9196c24bf1f
Dong <dlttthvlhhth@gmail.com>
2021-04-27 22:38:03 -0700

    add more detail for CartItem

--- src/CartItem.js
+++ src/CartItem.js
@@ -4,9 +4,20 @@ import styled from 'styled-components'
 function CartItem() {
     return (
         <Container>
-
+            <ImageContainer>
+                <img src={""} />
+            </ImageContainer>
+            <CartItemInfo>
+                <CartItemInfoTop></CartItemInfoTop>
+                <CartItemInfoBottom>
+                    <CartItemQuantity></CartItemQuantity>
+                    <CartItemDeleteContainer></CartItemDeleteContainer>
+                </CartItemInfoBottom>
+            </CartItemInfo>
+            <CartItemPrice></CartItemPrice>
         </Container>
     )
 }
+const Container = styled.div``;
 
 export default CartItem
--- src/CartItems.js
+++ src/CartItems.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import CartItem from './CartItem'
 function CartItems() {
     return (
         <Container>
@@ -20,5 +20,6 @@ flex:0.8;
 margin-right:18px;
 padding:20px
 `;
-
+const Title = styled.p``;
+const ItemContainer = styled.div``;
 export default CartItems

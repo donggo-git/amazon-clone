3025098504d2e12cb82bd050b7df7fee5a171dc6
Dong <dlttthvlhhth@gmail.com>
2021-04-29 16:57:10 -0700

    add style for cartItemInfoTop & CartItemInfoBottom

--- src/CartItem.js
+++ src/CartItem.js
@@ -22,19 +22,42 @@ function CartItem() {
         </Container>
     )
 }
-const Container = styled.div``;
+const Container = styled.div`
+padding-top:12px;
+padding-bottom: 12px;
+display:flex;
+`;
 const ImageContainer = styled.div`
     width:180px;
     height:180px;
+    flex-shrink:0;
+    flex-grow:0;
     img{
         object-fit:contain;
-        
+        height:100%;
+        width:100%;
     }
 `;
 const CartItemInfo = styled.div``;
-const CartItemInfoTop = styled.div``;
-const CartItemInfoBottom = styled.div``;
+const CartItemInfoTop = styled.div`
+color:#007185;
+h2{
+    font-size: 18px;
+}
+`;
+const CartItemInfoBottom = styled.div`
+display:flex;
+margin-top:4px;
+`;
 const CartItemQuantityContainer = styled.div``;
-const CartItemDeleteContainer = styled.div``;
-const CartItemPrice = styled.div``;
+const CartItemDeleteContainer = styled.div`
+color:#007185;
+margin-left:16px;
+cursor:pointer;
+`;
+const CartItemPrice = styled.div`
+font-size:18px;
+font-weight:700;
+margin-left:16px;
+`;
 export default CartItem

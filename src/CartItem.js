4a518a22667a01f6b5e73e5cedc6a957b009d8ff
Dong <dlttthvlhhth@gmail.com>
2021-04-28 23:32:28 -0700

    add style for ImageContainer

--- src/CartItem.js
+++ src/CartItem.js
@@ -8,16 +8,33 @@ function CartItem() {
                 <img src={""} />
             </ImageContainer>
             <CartItemInfo>
-                <CartItemInfoTop></CartItemInfoTop>
+                <CartItemInfoTop>
+                    <h2></h2>
+                </CartItemInfoTop>
                 <CartItemInfoBottom>
-                    <CartItemQuantity></CartItemQuantity>
-                    <CartItemDeleteContainer></CartItemDeleteContainer>
+                    <CartItemQuantityContainer>5</CartItemQuantityContainer>
+                    <CartItemDeleteContainer>Delete</CartItemDeleteContainer>
                 </CartItemInfoBottom>
             </CartItemInfo>
-            <CartItemPrice></CartItemPrice>
+            <CartItemPrice>
+                $1449
+            </CartItemPrice>
         </Container>
     )
 }
 const Container = styled.div``;
-
+const ImageContainer = styled.div`
+    width:180px;
+    height:180px;
+    img{
+        object-fit:contain;
+        
+    }
+`;
+const CartItemInfo = styled.div``;
+const CartItemInfoTop = styled.div``;
+const CartItemInfoBottom = styled.div``;
+const CartItemQuantityContainer = styled.div``;
+const CartItemDeleteContainer = styled.div``;
+const CartItemPrice = styled.div``;
 export default CartItem

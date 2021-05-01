8740c8d6c38f3477444472a32a4f78d1aca9b829
Dong <Dlttthvlhhth@gmail.com>
2021-04-30 23:12:31 -0700

    add getCartItems

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header'
 import Cart from './Cart'
 import Home from './Home'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 import styled from 'styled-components'
+import { db } from "./firebase"
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+  const getCartItems = () => {
+    db.collection("cartItems").onSnapshot((snapshot) => {
+      const tempItems = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        product: doc.data()
+      }))
+      setCartItems(tempItems)
+    })
+  }
+
+  useEffect(() => {
+    getCartItems();
+  }, [])
+  console.log(cartItems)
   return (
     <div className="App">
       <Router>

69a0202aa81b02c3937935843ea62028a503a8c0
Dong <Dlttthvlhhth@gmail.com>
2021-05-08 23:32:05 -0700

    create and add style for CheckoutButton

--- src/CartTotal.js
+++ src/CartTotal.js
@@ -3,7 +3,8 @@ import styled from 'styled-components'
 function CartTotal() {
     return (
         <Container>
-
+            <Subtotal>Subtotal</Subtotal>
+            <CheckoutButton>Proceed to checkout</CheckoutButton>
         </Container>
     )
 }
@@ -13,5 +14,19 @@ height:200px;
 background-color:white;
 flex:0.2;
 padding 20px
+`
+const CheckoutButton = styled.button`
+background-color:#f0c14b;
+width:100%;
+padding:4px 8px;
+border:2px solid #a88734;
+border-radius:4px;
+cursor:pointer;
+font-size:16px;
+:hover{
+    background:#ddb347;
+}
+`
+const Subtotal = styled.h2`
 `
 export default CartTotal
